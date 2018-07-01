cb3f8d6aa19d19c4ead3044d34aeb817c441cafa
Xingchen Hong <hello@xc-h.com>
2018-07-01 01:45:05 -0500

    Minor cleanup.

--- app/imports/api/checklists/collections.js
+++ app/imports/api/checklists/collections.js
@@ -1,12 +1,11 @@
-import { Mongo } from 'meteor/mongo';
+import {
+  Mongo,
+} from 'meteor/mongo';
 
 const baseName = 'checklists';
 const collection = new Mongo.Collection(baseName);
 
 export default collection;
-
-export
-const countCollection = new Mongo.Collection(`${baseName}.count`);
 
 export
 const selectors = {};
--- app/imports/api/checklists/publications.js
+++ app/imports/api/checklists/publications.js
@@ -1,64 +1,10 @@
 import {
   createPublication,
 } from '/imports/helpers.server';
-import Checklists, {
-  countCollection as ChecklistCount,
-  selectors,
-} from './collections';
+import Checklists from './collections';
 import {
   transformForIndex,
 } from './schema';
-
-// Publish the current size of the collection.
-createPublication('checklists.count', (pub) => {
-  const collectionName = ChecklistCount._name;
-  const docId = 'count';
-  let count = 0;
-  let initializing = true;
-
-  // `observeChanges` only returns after the initial `added` callbacks have run.
-  // Until then, we don't want to send a lot of `changed` messages—hence
-  // tracking the `initializing` state.
-  const handle = Checklists.find(selectors.initialized).observeChanges({
-    added() {
-      count += 1;
-
-      if (initializing) {
-        return;
-      }
-
-      pub.log(collectionName, 'added', count);
-
-      pub.changed(collectionName, docId, {
-        value: count,
-      });
-    },
-    removed() {
-      count -= 1;
-
-      pub.log(collectionName, 'removed', count);
-
-      pub.changed(collectionName, docId, {
-        value: count,
-      });
-    },
-  });
-
-  initializing = false;
-  pub.added(collectionName, docId, {
-    value: count,
-  });
-  pub.ready();
-  pub.onStop(() => handle.stop());
-});
-
-createPublication('checklists.all', (/* pub */) => {
-  return Checklists.find({}, {
-    sort: {
-      createDate: -1,
-    },
-  });
-});
 
 createPublication('checklists.index', (pub) => {
   const selector = {};
@@ -71,13 +17,13 @@ createPublication('checklists.index', (pub) => {
     fields,
   });
   const observer = cursor.observe({
-    added(document) {
+    added (document) {
       pub.added(Checklists._name, document._id, transformForIndex(document));
     },
-    changed(newDocument, oldDocument) {
+    changed (newDocument, oldDocument) {
       pub.changed(Checklists._name, oldDocument._id, transformForIndex(newDocument));
     },
-    removed(oldDocument) {
+    removed (oldDocument) {
       pub.removed(Checklists._name, oldDocument._id);
     },
   });
